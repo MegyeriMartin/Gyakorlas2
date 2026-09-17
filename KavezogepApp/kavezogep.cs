60a4a18c725b7b6023442a595b614b9501d1ca8c
MegyerIMartin <megyeri.martin.2022d@telekimezotur.hu>
2026-09-17 09:43:40 +0200

    kavegep

---
+++ KavezogepApp/kavezogep.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KavezogepApp
+{
+    internal class kavezogep
+    {
+        private int VizKeszletML;
+        public void VizTolt(int ml)
+        {
+            if(ml < 0)
+            {
+                VizKeszletML = 0;
+                Console.WriteLine("Hiba! Negativ erteket adtal meg");
+            }
+            else
+            {
+                VizKeszletML += ml;
+            }
+
+
+        }
+
+    public void KaveFozes()
+        {
+            if(VizKeszletML >= 150)
+            {
+                VizKeszletML -= 150;
+                Console.WriteLine("kave lefozve");
+            }
+            else
+            {
+                Console.WriteLine("Nem all rendelkesre eleg viz");
+            }
+        }
+    }
+}
