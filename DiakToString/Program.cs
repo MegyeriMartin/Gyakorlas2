9053c3f9da79e5c08a7c6c8f3e94f86e26a4f7bc
MegyerIMartin <megyeri.martin.2022d@telekimezotur.hu>
2026-09-17 09:35:27 +0200

    DiakToString

---
+++ DiakToString/Program.cs
@@ -0,0 +1,8 @@
+﻿using DiakToString;
+
+Diak diak1 = new Diak();
+Diak diak2 = new Diak("Nagy Árpi", 1.2);
+Diak diak3 = new Diak("Kiss Árpi");
+Console.WriteLine(diak1);
+Console.WriteLine(diak2);
+Console.WriteLine(diak3);
---
+++ DiakToString/diak.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace DiakToString
+{
+    internal class Diak
+    {
+        public string Nev { get; set; }
+        public double Atlag { get; set; }
+        public Diak()
+        {
+            Nev = "Nagy László Benedek";
+            Atlag = 4.5;
+        }
+        public Diak(string kapottnev, double kapottatlag)
+        {
+            Nev = kapottnev;
+            Atlag = kapottatlag;
+        }
+        public Diak(string csaknev)
+        {
+            Nev = csaknev;
+            Atlag = 4.3;
+        }
+        public override string ToString()
+        {
+            return $"Nev: {Nev}, Atlag {Atlag}";
+        }
+    }
+}
