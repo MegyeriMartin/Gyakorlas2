efa0079dfb35fdf1433564bee565b393699869a9
MegyerIMartin <megyeri.martin.2022d@telekimezotur.hu>
2026-09-17 08:47:31 +0200

    masodik feladat

---
+++ Homerseklet/Program.cs
@@ -0,0 +1,8 @@
+﻿using Homerseklet;
+
+HomersekletAtvalto peldany = new HomersekletAtvalto();
+peldany.Celsius = 25;
+Console.WriteLine(peldany.ToFahrenheit());
+
+double atadas = 30;
+Console.WriteLine(HomersekletAtvalto.CelsiusToFahrenheit(atadas));
