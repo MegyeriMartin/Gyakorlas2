0d93a41449faf22d5b0d5d9becc0eceeb0c72de6
MegyerIMartin <megyeri.martin.2022d@telekimezotur.hu>
2026-09-17 08:35:55 +0200

    elso feladat

---
+++ FilmApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using FilmApp;
+
+film elsofilm = new film();
+elsofilm.Cim = "A Gyuruk ura";
+elsofilm.HosszPercben = 190;
+
+film masodikfilm = new film();
+Console.WriteLine("adja meg a masodik film cimet");
+masodikfilm.Cim = Console.ReadLine();
+Console.WriteLine("Adja meg a masodik film hosszat");
+masodikfilm.HosszPercben = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"az elso film cime: {elsofilm.Cim}, az elso film hossza: {elsofilm.HosszPercben}.");
+Console.WriteLine($"az elso film cime: {masodikfilm.Cim}, az elso film hossza: {masodikfilm.HosszPercben}.");
---
+++ FilmApp/film.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace FilmApp
+{
+    internal class film
+    {
+
+        public string Cim;
+        public int HosszPercben;
+    }
+}
