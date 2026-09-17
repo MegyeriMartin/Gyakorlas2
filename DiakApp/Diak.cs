42329e4563dfd20f9bc452fcd0cf2bcbfe5becf3
MegyerIMartin <megyeri.martin.2022d@telekimezotur.hu>
2026-09-17 09:23:01 +0200

    konstruktor

---
+++ DiakApp/Diak.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace DiakApp
+{
+    internal class Diak
+    {
+        public string Nev { get; set; }
+        public double Atlag { get; set; }
+        public Diak()
+        {
+            Nev = "Nagy László Benedek";
+            Atlag = 4.5;
+        }
+        public Diak(string kapottnev, double kapottatlag)
+        {
+            Nev = kapottnev;
+            Atlag = kapottatlag;
+        }
+        public Diak(string csaknev)
+        {
+            Nev = csaknev;
+            Atlag = 4.3;
+        }
+    }
+}
---
+++ DiakApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using DiakApp;
+Diak parameternelkul = new Diak();
+Diak egyparameteres = new Diak("Nagy Árpi");
+Diak ketparameteres = new Diak("Kis árpi", 3.6);
+
+Console.WriteLine($"Parameter nelkuli konstruktor: {parameternelkul.Nev}, Atlag:{parameternelkul.Atlag}");
+Console.WriteLine($"Egyparameteres konstruktor: {egyparameteres.Nev}, Atlag:{egyparameteres.Atlag}");
+Console.WriteLine($"Ketparameteres konstruktor: {ketparameteres.Nev}, Atlag:{ketparameteres.Atlag}");
